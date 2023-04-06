1a4512b762fe0db08d818e2c615e6018950425c7
dfsalam <dfsalam@gmail.com>
2023-04-06 17:12:22 -0500

    Create all? and filter methods

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,12 +1,15 @@
 module MyEnumerable
-  def all?
-    puts("All_method")
+  def all?(&block)
+    each { |item| return false unless block.call(item) }
+    true
   end
   def any?(&block)
     each { |item| return true if block.call(item) }
     false
   end
-  def filter?
-    puts("Filter_method")
+  def filter(&block)
+    result = []
+    each { |item| result << item if block.call(item) }
+    result
   end
 end
